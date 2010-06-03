54f533c592b1c6e58d01e3d67dc8f5057017aceb
shybyte <shybyte@ae312e1e-15fd-9845-32bd-2af5d6c33534>
2010-06-03 19:50:47 +0000

    added backgroundpage mockup in popup.js

--- encrypt-the-cloud/chrome-extension/popup.js
+++ encrypt-the-cloud/chrome-extension/popup.js
@@ -1,22 +1,43 @@
 $(function(){
-	$("#accordion").accordion();
+    $("#accordion").accordion();
     loadState();
     $('#save').click(function(){
-    	saveState();
-		window.close()
+        saveState();
+        window.close()
     })
 });
 
 
-function saveState() {
-	var state = chrome.extension.getBackgroundPage().getState();
-	state.password = $("#password").val();
-	state.username = $("#username").val();
-	chrome.extension.getBackgroundPage().setState(state);
+function saveState(){
+    var state = chrome.extension.getBackgroundPage().getState();
+    state.password = $("#password").val();
+    state.username = $("#username").val();
+    chrome.extension.getBackgroundPage().setState(state);
 }
 
-function loadState() {
-	var state = chrome.extension.getBackgroundPage().getState();
-	$("#username").val(state.username);
-	$("#password").val(state.password);
+function loadState(){
+    var state = chrome.extension.getBackgroundPage().getState();
+    $("#username").val(state.username);
+    $("#password").val(state.password);
+}
+
+// mock background page
+if (!chrome.extension) {
+    chrome.extension = {
+        getBackgroundPage: function(){
+            var state = {
+                username: "shybyte",
+                password: "secret",
+            };
+            return {
+                setState: function(newState){
+                    state = newState;
+                },
+                getState: function(){
+                    return state;
+                }
+                
+            }
+        }
+    };
 }
