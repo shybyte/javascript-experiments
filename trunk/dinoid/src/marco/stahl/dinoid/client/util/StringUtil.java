0adc1064a311338294d54131c9d9985884fc07e7
shybyte <shybyte@ae312e1e-15fd-9845-32bd-2af5d6c33534>
2010-07-20 22:30:12 +0000

    removed warnings

--- trunk/dinoid/src/marco/stahl/dinoid/client/util/StringUtil.java
+++ trunk/dinoid/src/marco/stahl/dinoid/client/util/StringUtil.java
@@ -1,11 +1,11 @@
 package marco.stahl.dinoid.client.util;
 
 public class StringUtil {
-	public static final String ensureNotEmpty(String s, String defaultString) {
+	public static final String ensureNotEmpty(final String s, final String defaultString) {
 		return isEmtpty(s) ? defaultString : s;
 	}
 
-	private static boolean isEmtpty(String s) {
+	private static boolean isEmtpty(final String s) {
 		return s == null || s.trim().length() == 0;
 	}
 }
