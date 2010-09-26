70035d1c72186246f36dd5747cad3da01e01f374
shybyte <shybyte@ae312e1e-15fd-9845-32bd-2af5d6c33534>
2010-09-26 09:54:59 +0000

    playing around with optimization.

---
+++ trunk/week-day-trainer/js/minify.sh
@@ -0,0 +1,4 @@
+java -jar /home/shybyte/opt/closurec/compiler.jar --js jquery-ui-1.8.5.custom.min.js --js flot/jquery.flot.min.js --js jquery.json-2.2.min.js  --js nano.js --js jstorage.js --js script.js  --js_output_file script.min.js
+java -jar /home/shybyte/opt/closurec/compiler.jar --js gears_init.js --js_output_file gears_init.min.js 
+#java -jar /home/shybyte/opt/closurec/compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS  --js script.js --js_output_file script.min.js 
+# start: 184.23
